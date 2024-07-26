b588da6f70aecdaccff8ca12fd32a9d1e19f3558
wlsql852 <wlsql852@naver.com>
2024-07-26 12:58:53 +0900

    level2-7

--- src/main/java/level2/calculator/App.java
+++ src/main/java/level2/calculator/App.java
@@ -1,38 +1,56 @@
 package level2.calculator;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         /* Calculator 인스턴스 생성 */
         Calculator cal = new Calculator();
-        //cal.setResultArray(new ArrayList<Integer>());
         Scanner sc = new Scanner(System.in);
 
         /* 반복문 시작 */
         String stop = "continue";
         while(!stop.equals("exit")) {
-            System.out.print("첫 번째 숫자를 입력하세요:");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요:");
-            int num2 = sc.nextInt();
+            System.out.println("계산 유형을 선택하세요(circle 입력 시 원의 넓이 구하기, 그 외는 사칙연산)");
+            String celtype = sc.next();
+            if (celtype.equals("circle")) {
+                System.out.print("반지름 길이를 입력하세요:");
+                double radius = sc.nextDouble();
+                double width = cal.calculateCircleArea(radius);
+                System.out.println("결과 : " + width);
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
-            //계산
-            int result = cal.calculate(operator, num1, num2);
-            System.out.println("결과 : " + result);
+                //검색기록 조회
+                System.out.println("결과기록을 보시겠습니까? (show 입력 시 검색기록 출력)");
+                String resultLog = sc.next();
+                if (resultLog.equals("show")) cal.selectCircleArray();
 
-            //검색기록 조회
-            System.out.println("결과기록을 보시겠습니까? (show 입력 시 검색기록 출력)");
-            String resultLog = sc.next();
-            if (resultLog.equals("show")) cal.inquiryResults();
+                //첫번째 검색 기록 삭제
+                System.out.println("첫번째 결과기록을 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.next();
+                if(remove.equals("remove")) cal.removeWidth();
 
-            //첫번째 검색 기록 삭제
-            System.out.println("첫번째 결과기록을 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String remove = sc.next();
-            if(remove.equals("remove")) cal.removeResult();
+            }else {
+                System.out.print("첫 번째 숫자를 입력하세요:");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요:");
+                int num2 = sc.nextInt();
+
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
+                //계산
+                int result = cal.calculate(operator, num1, num2);
+                System.out.println("결과 : " + result);
+
+                //검색기록 조회
+                System.out.println("결과기록을 보시겠습니까? (show 입력 시 검색기록 출력)");
+                String resultLog = sc.next();
+                if (resultLog.equals("show")) cal.inquiryResults();
+
+                //첫번째 검색 기록 삭제
+                System.out.println("첫번째 결과기록을 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.next();
+                if(remove.equals("remove")) cal.removeResult();
+            }
 
             //반복 유지할지 결정
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
--- src/main/java/level2/calculator/Calculator.java
+++ src/main/java/level2/calculator/Calculator.java
@@ -3,9 +3,13 @@ package level2.calculator;
 import java.util.ArrayList;
 
 public class Calculator {
-    ArrayList<Integer> resultArray;
+    private ArrayList<Integer> resultArray;
+    private ArrayList<Double> circleArray;
+    private static final double PI = 3.14;
+
     public Calculator() {
         resultArray = new ArrayList<>();
+        circleArray = new ArrayList<>();
     }
 
     /* Getter 메서드 구현 */
@@ -13,33 +17,44 @@ public class Calculator {
         return resultArray;
     }
 
+    /* Setter 메서드 구현 */
     public void setResultArray(ArrayList<Integer> Array) {
         this.resultArray = Array;
     }
 
-    /* Setter 메서드 구현 */
+    /* 원의 넓이 저장 필드 Getter, Setter 메서드 구현 */
+
+    public ArrayList<Double> getCircleArray() {
+        return circleArray;
+    }
+
+    public void setCircleArray(ArrayList<Double> circleArray) {
+        this.circleArray = circleArray;
+    }
+
+
     public int calculate(char operator, int num1, int num2) {
         int result=0;
         switch (operator) {
             case '+':
                 result = num1 + num2;
-                System.out.println("결과: " + result);
+                //System.out.println("결과: " + result);
                 resultArray.add(result);
                 break;
             case '-':
                 result = num1 - num2;
-                System.out.println("결과: " + result);
+                //System.out.println("결과: " + result);
                 resultArray.add(result);
                 break;
             case '*':
                 result = num1 * num2;
-                System.out.println("결과: " + result);
+                //System.out.println("결과: " + result);
                 resultArray.add(result);
                 break;
             case '/':
                 try {
                     result = num1 / num2;
-                    System.out.println("결과: " + result);
+                    //System.out.println("결과: " + result);
                     resultArray.add(result);
                 } catch (ArithmeticException e) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
@@ -58,5 +73,23 @@ public class Calculator {
     //저장된 연산 결과들을 조회하는 기능을 가진 메서드
     public void inquiryResults() {
         System.out.println("이전 검색 결과 : "+resultArray.toString());
+    }
+
+    /* 원의 넓이를 구하는 메서드 선언*/
+    public double calculateCircleArea(double radius) {
+        /* 원의 넓이 계산 구현 */
+        double circleWidth =radius*radius*PI;
+        circleArray.add(circleWidth);
+        return circleWidth;
+    }
+    /* 원의 넓이 저장 필드 조회 메서드 구현 */
+    public void selectCircleArray() {
+        System.out.println("원의 넓이 계산 결과 기록 : "+ circleArray.toString());
+    }
+    /* 원의 넓이 저장 필드 첫번째 값 삭제 메서드 구현 */
+    public void removeWidth() {
+        circleArray.remove(0);
+        System.out.println("삭제되었습니다.");
+        System.out.println("원의 넓이 계산 결과 기록 : "+circleArray.toString());
     }
 }
